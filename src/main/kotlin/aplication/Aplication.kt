c8f3feedc4073ab86355f66ba5d875e66f6e5788
blancoparis <blancoparis@gmail.com>
2016-08-04 20:21:41 +0200

    Configuracion de mysql
    Configuración de mysql

--- src/main/kotlin/aplication/Aplication.kt
+++ src/main/kotlin/aplication/Aplication.kt
@@ -1,12 +1,18 @@
 package aplication
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 /**
  * Created by davidTorre on 19/07/2016.
  */
 @SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 open class Application {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
